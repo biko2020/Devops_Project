af85e7ddb6e3e3bfb5287c515e9b6cfb14214170
biko2020 <brahimlion38@hotmail.com>
2022-02-02 16:50:35 +0100

    ajouter l'objet CustomerRequestDTO

---
+++ src/main/java/org/openlab/openlapcustomerservice/dto/CustomerRequestDTO.java
@@ -0,0 +1,10 @@
+package org.openlab.openlapcustomerservice.dto;
+
+import lombok.Data;
+
+@Data
+public class CustomerRequestDTO {
+    private String id;
+    private String name;
+    private String email;
+}
