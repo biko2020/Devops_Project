ae87c7aa3856497646c493da23836814730a7d76
biko2020 <brahimlion38@hotmail.com>
2022-02-02 16:43:41 +0100

    ajouter la class Customer de notre entities jpa

---
+++ src/main/java/org/openlab/openlapcustomerservice/OpenlapCustomerServiceApplication.java
@@ -0,0 +1,13 @@
+package org.openlab.openlapcustomerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenlapCustomerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenlapCustomerServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/openlab/openlapcustomerservice/OpenlapCustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.openlab.openlapcustomerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenlapCustomerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
