5cfc1388c302e9793043f4a72e9c2f395f391cff
biko2020 <brahimlion38@hotmail.com>
2022-02-02 17:31:46 +0100

    ajouter l'interface CustomerMapper

---
+++ src/main/java/org/openlab/openlapcustomerservice/mappers/CustomerMapper.java
@@ -0,0 +1,14 @@
+package org.openlab.openlapcustomerservice.mappers;
+
+import org.mapstruct.Mapper;
+import org.openlab.openlapcustomerservice.dto.CustomerRequestDTO;
+import org.openlab.openlapcustomerservice.dto.CustomerResponseDTO;
+import org.openlab.openlapcustomerservice.entities.Customer;
+
+@Mapper(componentModel = "spring")
+public interface CustomerMapper {
+
+    CustomerResponseDTO customerToCustomerResponseDTO(Customer customer);
+    Customer customerResponseDTOCustomer(CustomerRequestDTO customerRequestDTO);
+
+}
