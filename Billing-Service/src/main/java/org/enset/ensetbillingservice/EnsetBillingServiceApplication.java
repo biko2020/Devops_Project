5e3bb6fb3625e89d85382213a73953de35986128
biko2020 <brahimlion38@hotmail.com>
2022-02-05 10:29:51 +0100

    ajouter le projet  billing Service

---
+++ Billing-Service/src/main/java/org/enset/ensetbillingservice/EnsetBillingServiceApplication.java
@@ -0,0 +1,13 @@
+package org.enset.ensetbillingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnsetBillingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnsetBillingServiceApplication.class, args);
+    }
+
+}
