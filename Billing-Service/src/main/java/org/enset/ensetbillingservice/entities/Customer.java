8c507500c2bfe30e2065d06e7d92ea8eab0cd6e3
biko2020 <brahimlion38@hotmail.com>
2022-02-05 14:29:33 +0100

    ajouter la class InvoiceRestController et les methodes getInvoice(), getInvoiceByCustomer() pour la couche web

--- Billing-Service/src/main/java/org/enset/ensetbillingservice/entities/Customer.java
+++ Billing-Service/src/main/java/org/enset/ensetbillingservice/entities/Customer.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 
 public class Customer {
 
-// les proprietes de la class Customer
+// les parametres de la class Customer
     private String id;
     private String name;
     private  String email;
--- Billing-Service/src/main/java/org/enset/ensetbillingservice/web/InvoiceRestController.java
+++ Billing-Service/src/main/java/org/enset/ensetbillingservice/web/InvoiceRestController.java
@@ -1,2 +1,31 @@
-package org.enset.ensetbillingservice.web;public class InvoiceRestController {
+package org.enset.ensetbillingservice.web;
+
+import org.enset.ensetbillingservice.dto.InvoiceResponseDTO;
+import org.enset.ensetbillingservice.services.InvoiceService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path="/api")
+
+public class InvoiceRestController {
+
+    private InvoiceService invoiceService;
+    public InvoiceRestController(InvoiceService invoiceService) {
+        this.invoiceService = invoiceService;
+    }
+
+        @GetMapping(path = "/invoices/{id}")
+        public InvoiceResponseDTO getInvoice(@PathVariable(name = "id")String invoiceId){
+              return invoiceService.getInvoice(invoiceId);
+        }
+
+    @GetMapping(path = "/invoices/{customerId}")
+    public List<InvoiceResponseDTO> getInvoiceByCustomer(@PathVariable String customerId){
+        return invoiceService.invoiceByCustomerId(customerId);
+    }
 }
